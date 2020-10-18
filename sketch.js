81933e47e290f83a92aa1ca586ee6b1f67cb519f
Harshit Gupta <mail4harshit1@gmail.com>
2020-10-18 18:54:51 +0530

    basic functionalities

---
+++ sketch.js
@@ -0,0 +1,61 @@
+let originalheight = 1;
+let originalwidth = 1;
+let init_time = (new Date()).getTime();
+let cur_time = (new Date()).getTime();
+let dff_time = (new Date()).getTime();
+var mouse = [];
+var keyboard = [];
+var record = false;
+var sound;
+var recorder;
+var pdf = null;
+sound = new Howl({
+    src: [localStorage.getItem("audio")]
+});
+
+function setup() {
+    let cnt = createCanvas(canvasWidth, canvasHeight);
+    cnt.parent('mouse');
+    // background(220);
+    // beginRendering();
+}
+
+function draw() {
+    clear();
+    ellipse(mouseX, mouseY, 10, 10);
+    // console.log(mouseX)
+    fill('blue');
+    stroke('blue');
+    if (record) {
+        if (cur_time - diff_time > 100) {
+            mouse.push([(cur_time - init_time) / 1000, [mouseX, mouseY]]);
+            // mouse_y.push(mouseY);
+            // mouse_time.push((cur_time - init_time) / 1000);
+            diff_time = cur_time;
+        }
+        cur_time = (new Date()).getTime();
+    }
+}
+
+startrecording = async() => {
+    record = record ? false : true;
+    btn = document.getElementById('record');
+    btn.innerHTML = record ? "Stop" : "Record";
+    init_time = (new Date()).getTime();
+    diff_time = (new Date()).getTime();
+    if (record) {
+        recorder = await recordAudio();
+        recorder.start();
+        mouse = [];
+        keyboard = [];
+    }
+    if (!record) {
+        sound = await recorder.stop();
+        // sound.play();
+        mouseData = JSON.stringify(mouse)
+        keyboardData = JSON.stringify(keyboard)
+        getSLD(mouseData, keyboardData, pdf, sound.audioBlob)
+    }
+}
+
+document.getElementById('record').addEventListener('click', startrecording)
